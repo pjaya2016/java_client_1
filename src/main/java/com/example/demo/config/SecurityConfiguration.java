f6308f265c018d39f047635f9baf15551229d417
prekjgnan <pjayamayooragan2016@gmail.com>
2022-06-12 22:34:19 +0100

    First commit

---
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+
+}
