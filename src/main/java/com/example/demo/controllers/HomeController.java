364ac5cae493cbb79b007e31f0a0b858e08677eb
prekjgnan <pjayamayooragan2016@gmail.com>
2022-06-13 15:21:11 +0100

    Added auth to get user details

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping("/test")
-    public String test(){
-       return "Hello world";
+    public Authentication test(){
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 }
